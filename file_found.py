714fd1a9a1d9f88c28a5ccaaec8734d6563753b2
ljaag <luisjaag@gmail.com>
2025-06-03 19:52:41 +0200

    Delete file_found.py

--- file_found.py
+++
@@ -1,9 +0,0 @@
-import os
-from kbeutils.data import airfoils
-from parapy.core.validate import AdaptedValidator
-
-
-file_found = AdaptedValidator(lambda name: os.path.isfile(os.path.join(airfoils.__path__[0],
-                                                                       name + ".dat")))
-# This adapted validator checks if the file exists in the airfoils folder of kbeutils, avoiding
-# crashes when the file is not found because of a wrong input.
