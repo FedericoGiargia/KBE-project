65ffbae677ac3b40129f801c4fb9f432bf167ced
luisjavier-tec <luisjavier.ag@tec.mx>
2025-06-08 12:09:18 -0600

    Trying for AVL Implementation

---
+++ fede/convAVL.py
@@ -0,0 +1,9 @@
+import os
+from math import radians, tan
+from kbeutils import avl
+from parapy.geom import GeomBase, translate, rotate, MirroredShape
+from parapy.core import Input, Attribute, Part
+from fede import LiftingSurface, Fuselage, Frame, file_found, Booms, Blade, Propeller, LiftingLandingGear, LandingGear, Winglet
+from fede.GUI_Test import *
+
+Aera_A = Aera
